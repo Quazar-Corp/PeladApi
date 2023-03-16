dfa62a78e7544cb7206ce53909502cdfc10ec801
OnofreTZK <taraujonofre@gmail.com>
2023-03-16 19:45:35 -0300

    feat: started lein project

---
+++ src/pelada/core.clj
@@ -0,0 +1,6 @@
+(ns pelada.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/pelada/core_test.clj
@@ -0,0 +1,7 @@
+(ns pelada.core-test
+  (:require [clojure.test :refer :all]
+            [pelada.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
