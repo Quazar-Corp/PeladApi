942711d5bf3d42ad27ab8910633adcd26a450987
OnofreTZK <taraujonofre@gmail.com>
2023-03-16 21:52:44 -0300

    feat: get next wednesday and list generation

--- src/pelada/core.clj
+++ src/pelada/core.clj
@@ -1,6 +1,44 @@
 (ns pelada.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-next-wednesday
+ "Retrieve the formatted date of the next Wednesday" 
+  []
+  (let [now (java.time.LocalDate/now (java.time.ZoneId/of "GMT-3"))
+        current-day-of-week (.getDayOfWeek now)
+        ; 7+3 (current + number of days for the next we) - current day 
+        days-to-next-wednesday (- 10 (.getValue current-day-of-week))
+        ; Math/max ensure that the number of days is positive
+        next-wednesday (.plusDays now (Math/max days-to-next-wednesday 0))
+        formatter (java.time.format.DateTimeFormatter/ofPattern "dd/MM")]
+    (.format next-wednesday formatter)))
+
+(def default_owner "Onofre")
+(def default_hour "20hrs - 22hrs")
+
+(defn list-string
+  "Format the string with the given values"
+  [date hour owner]
+   (str "*Lista pelada Quarta " date
+        " PN Boulevard, " hour
+        "*\n\n"
+        "*Goleiros*\n" "1.\n2."
+        "\n\n"
+        "*Jogadores*\n" 
+        "1. " owner
+        "\n\n"
+        "*Suplentes*\n1."
+        "\n\n"
+        "*Convidados*\n1."
+  ))
+
+(defn weekly-list
+  "Generate the weekly list for the pelada"
+  ([] (list-string (get-next-wednesday) default_hour default_owner))
+  ([date] (list-string date default_hour default_owner))
+  ([date hour] (list-string date hour default_owner)))
+
+(println (weekly-list))
+
+(defn -main
+  [& args]
+  )
