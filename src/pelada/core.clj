b3c0edc7ee575b8d399beca5b09666e4d6dee3da
OnofreTZK <taraujonofre@gmail.com>
2023-03-21 17:02:46 -0300

    feat: team maker first version

--- src/pelada/core.clj
+++ src/pelada/core.clj
@@ -8,6 +8,10 @@
 
 (println "\n\n>>> Reading sample.dat\n\n")
 (println (list-handler/parser "./sample.dat"))
+(println "\n")
+(let [pelada (manager/to-pelada-object "./sample.dat/")
+      [t1 t2 t3] (manager/team-maker pelada)]
+  (println (format "Time 1:\n%s\n\nTime 2:\n%s\n\nTime 3:\n%s" t1 t2 t3)))
 
 (defn -main
   [& args]
--- src/pelada/pelada_manager.clj
+++ src/pelada/pelada_manager.clj
@@ -3,7 +3,22 @@
 
 (defn to-pelada-object
   [filepath]
-  list-handler/parser filepath)
+  (list-handler/parser filepath))
+
+(defn team-maker-random
+  [acc players]
+  (if (empty? players)
+    [acc players]
+    (let [chosen-player (rand-nth players)
+          team (cons chosen-player acc)
+          updated-players (remove #(= chosen-player %) players)]
+      (if (= (count team) 5)
+        [team updated-players]
+        (team-maker-random team updated-players)))))
 
 (defn team-maker
-  [])
+  [pelada]
+  (let [[first-team players-list-1] (team-maker-random '() (:players pelada))  
+        [second-team players-list-2] (team-maker-random '() players-list-1)
+        [third-team _] (team-maker-random '() players-list-2)]
+    [first-team second-team third-team]))
