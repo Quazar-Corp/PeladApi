d271fb0438ecfc91dc9db245582f581acb449e1e
OnofreTZK <taraujonofre@gmail.com>
2023-04-04 22:32:39 -0300

    feat: deploy script

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject pelada "0.1.0-SNAPSHOT"
+(defproject pelada ""
+  :uberjar-name "pelada-bin.jar"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.match "1.0.1"]]
-  :main ^:skip-aot pelada.core
+  :main pelada.core
+  :profiles {:uberjar {:aot :all}}
   :repl-options {:init-ns pelada.core})
